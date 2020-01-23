d50505c1ded18a3ec19b987b6f95730dd4c93c26
Spencer Wyman <spencer.wyaman7@gmail.com>
2020-01-23 15:19:16 -0800

    watchmo watch command will send every query in the config file to the appropriate endpoint

--- src/commands/watch.js
+++ src/commands/watch.js
@@ -1,18 +1,24 @@
 const fs = require('fs');
+const { request } = require('graphql-request');
 
-const timeTrackWrapper = (queryCallback) => () => {
-  const start = process.hrtime();
-  queryCallback()
-  .then(response => {
-  const timing = process.hrtime(start);
-  return { response, timing };
-  })
-}
+// const timeTrackWrapper = (queryCallback) => () => {
+//   const start = process.hrtime();
+//   queryCallback()
+//   .then(response => {
+//   const timing = process.hrtime(start);
+//   return { response, timing };
+//   })
+// }
+
 // IN DEVELOPMENT, NOT FOR PRODUCTION
 function watch(endpoint, categories) {
-  console.log('endpoint', endpoint);
-  console.log('categories', categories);
-  console.log('time', Date.now());
+  for (let cat in categories) {
+    for (query of categories[cat].queries) {
+      request(endpoint, query)
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err))
+    }
+  }
 }
 
 module.exports = { watch };
