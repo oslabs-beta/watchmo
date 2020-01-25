f4e0dd3a2cf74a497c7d82003bed8987586f8c7a
Spencer Wyman <spencer.wyaman7@gmail.com>
2020-01-25 10:37:53 -0800

    commented watch.js and cleaned up init.js

--- src/commands/watch.js
+++ src/commands/watch.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const path = require('path');
 const { request } = require('graphql-request');
 
+// returns a promise that resolves to the response/timing object to be saved
 const buildQueryPromise = (endpoint, query) => new Promise((resolve, reject) => {
   const start = process.hrtime();
   request(endpoint, query)
@@ -13,6 +14,7 @@ const buildQueryPromise = (endpoint, query) => new Promise((resolve, reject) => 
   })
 })
 
+//helper function for saving data to an appropriate path
 function saveData(data, savePath) {
   fs.writeFile(savePath, JSON.stringify(data), (err) => {
     if (err) {
@@ -23,6 +25,9 @@ function saveData(data, savePath) {
   });
 }
 
+// This function waits for the query Promise to resolve to the appropriate data for each query.
+// This ensures that the timer starting and stopping in the Promise itself is starting and stopping at the appropriate times
+// Finally, it saves the data built up from each query and saves it in the the given path as a JSON file.
 async function sendQueriesAndSave(endpoint, category, dirPath) {
   const timingInfo = {};
   let responseObject;
@@ -36,6 +41,9 @@ async function sendQueriesAndSave(endpoint, category, dirPath) {
   saveData(timingInfo, savePath);
 }
 
+// sets an interval for each category of query
+// Promises resolve with priority over setInterval, so the timing data isn't affected
+// We may want this to be a cron job or something else in the future
 function watch(endpoint, categories, dirPath) {
   for (let cat in categories) {
     setInterval(() => sendQueriesAndSave(endpoint, categories[cat], dirPath), categories[cat].frequency);
