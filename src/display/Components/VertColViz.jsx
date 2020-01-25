fcbb47ecdeb1e54069d7394e42069eb03454a27e
Sarah's Mac <sarahsmac@Apples-Mac-4.local>
2020-01-25 11:41:20 -0800

    updated notes for team

--- src/display/Components/VertColViz.jsx
+++ src/display/Components/VertColViz.jsx
@@ -15,7 +15,16 @@ function VertColViz() {
     const svgRef = useRef();
 
     // will be called initially and on every data change
+     /* Scalar Vector Graphics is an open web standard that uses a structured text format (XML) for rendering vector graphics.
+
+The most basic SVG file contains the following format:
+
+--Size of the viewport (Think of this as the image resolution)
+--Grouping instructions via the element -- How should elements be grouped?
+--Drawing instructions using the shapes elements
+--Style specifications describing how each element should be drawn.*/
     useEffect(() => {
+       
         const svg = select(svgRef.current);
         const xScale = scaleBand()
             .domain(data.map((value, index) => index))
@@ -26,18 +35,19 @@ function VertColViz() {
             .domain([0, 150])
             .range([150, 0]);
 
+            //changes the color depending on scale of data
         const colorScale = scaleLinear()
-            .domain([75, 100, 150])
-            .range(["green", "orange", "red"])
+            .domain([20, 40, 60, 80, 100, 120])
+            .range(["red", "orange", "yellow", "green", "blue", "pink"])
             .clamp(true);
-
+//defines the x axis & style
         const xAxis = axisBottom(xScale).ticks(data.length);
 
         svg
             .select(".x-axis")
             .style("transform", "translateY(150px)")
             .call(xAxis);
-
+//defines the y axis
         const yAxis = axisRight(yScale);
         svg
             .select(".y-axis")
@@ -52,6 +62,7 @@ function VertColViz() {
 
             .style("transform", "scale(1, -1)")
             .attr("x", (value, index) => xScale(index))
+            //location of bars, the higher the number, the higher the position on the graph
             .attr("y", -150)
             .attr("width", xScale.bandwidth())
             .transition()
@@ -59,6 +70,8 @@ function VertColViz() {
             .attr("height", value => 150 - yScale(value));
     }, [data]);
     return (
+        /*React fragments let you group a list of children without adding extra nodes to the DOM 
+        because fragments are not rendered to the DOM. */
         <React.Fragment>
             <svg ref={svgRef}>
                 <g className="x-axis" />
