10df32fd23f8cff0c4d78e02b0e79e829133446c
Jason Jones <jasonroyjones@gmail.com>
2020-01-25 14:01:38 -0800

    fixed child key error for categories

--- src/display/Components/UserDashboard.tsx
+++ src/display/Components/UserDashboard.tsx
@@ -5,52 +5,51 @@ import VertColViz from './VertColViz';
 
 //typescript: testing heading and caption
 const UserDashboard: React.FC = () => {
-    //setting the state for the drop down button with typescript
-    const [dropdownCatOpen, setCatOpen] = React.useState<boolean>(false);
-    const [dropdownDateOpen, setDateOpen] = React.useState<boolean>(false);
+  //setting the state for the drop down button with typescript
+  const [dropdownCatOpen, setCatOpen] = React.useState<boolean>(false);
+  const [dropdownDateOpen, setDateOpen] = React.useState<boolean>(false);
 
-    //function that is in charge of changing the state
-    const toggleCat = (): void => {
-        setCatOpen(!dropdownCatOpen);
-    }
-    const toggleDate = (): void => {
-        setDateOpen(!dropdownDateOpen);
-    }
-    const arrayOfCategories: string [] = ["Cat 1", "Cat 2", "Cat 3"];
-    const categories: object[] = [];
-    for (let i = 0; i < arrayOfCategories.length; i++) {
-        categories.push(<DropdownItem > { arrayOfCategories[i]} </DropdownItem>)
-    }
-    return (
-        <div id='UserDashboard'>
-            <h1> User Dashboard  </h1>
-            <button className="configButton">CONFIG</button>
-            <div className="categoriesDrop">
-                <ButtonDropdown isOpen={dropdownCatOpen} toggle={toggleCat}>
-                    <DropdownToggle caret color="primary">
-                        Catagories:
-  </DropdownToggle>
-                    <DropdownMenu>
-                        {categories}
-                    </DropdownMenu>
-                </ButtonDropdown>
+  //function that is in charge of changing the state
+  const toggleCat = (): void => {
+    setCatOpen(!dropdownCatOpen);
+  };
+  const toggleDate = (): void => {
+    setDateOpen(!dropdownDateOpen);
+  };
+  const arrayOfCategories: string[] = ['Cat 1', 'Cat 2', 'Cat 3'];
+  const categories: object[] = [];
+  for (let i = 0; i < arrayOfCategories.length; i++) {
+    categories.push(<DropdownItem key={i}> {arrayOfCategories[i]} </DropdownItem>);
+  }
+  return (
+    <div id="UserDashboard">
+      <h1> User Dashboard </h1>
+      <button className="configButton">CONFIG</button>
+      <div className="categoriesDrop">
+        <ButtonDropdown isOpen={dropdownCatOpen} toggle={toggleCat}>
+          <DropdownToggle caret color="primary">
+            Catagories:
+          </DropdownToggle>
+          <DropdownMenu>{categories}</DropdownMenu>
+        </ButtonDropdown>
 
-                <ButtonDropdown isOpen={dropdownDateOpen} toggle={toggleDate}>
-                    <DropdownToggle caret color="info">
-                        Dates:
-  </DropdownToggle>
-                    <DropdownMenu>
-                        <DropdownItem>Date 1</DropdownItem>
-                        <DropdownItem> Date 2 </DropdownItem>
-                        <DropdownItem> Date 3 </DropdownItem>
-                        <DropdownItem> Date 4 </DropdownItem>
-                    </DropdownMenu>
-                </ButtonDropdown></div>
-            <div>
-                <VertColViz />
-            </div>
-        </div>
-    )
-}
+        <ButtonDropdown isOpen={dropdownDateOpen} toggle={toggleDate}>
+          <DropdownToggle caret color="info">
+            Dates:
+          </DropdownToggle>
+          <DropdownMenu>
+            <DropdownItem key={1}>Date 1</DropdownItem>
+            <DropdownItem key={2}> Date 2 </DropdownItem>
+            <DropdownItem key={3}> Date 3 </DropdownItem>
+            <DropdownItem key={4}> Date 4 </DropdownItem>
+          </DropdownMenu>
+        </ButtonDropdown>
+      </div>
+      <div>
+        <VertColViz />
+      </div>
+    </div>
+  );
+};
 
 export default UserDashboard;
