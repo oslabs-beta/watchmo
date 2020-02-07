780ea080dabb7276366e0849b7bfd5374a5cb76e
EH1537 <ehilton1537@gmail.com>
2020-02-06 19:41:21 -0800

    front end works with new CLI framedwork

--- src/display/index.jsx
+++ src/display/index.jsx
@@ -1,5 +1,11 @@
 import * as React from 'react';
 import * as ReactDom from 'react-dom';
-import App from './Components/App';
+import { App } from './Components/App';
+import { ProjectProvider } from './Components/Context/ProjectContext';
 
-ReactDom.render(<App />, document.getElementById('root'));
+ReactDom.render(
+  <ProjectProvider>
+    <App />
+  </ProjectProvider>, 
+  document.getElementById('root'));
+
