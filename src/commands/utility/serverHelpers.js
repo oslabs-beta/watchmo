e203a4b7b7163809bac26f80be7eb3bdafb3252e
Sarah's Mac <sarahsmac@Apples-Mac-4.local>
2020-02-10 15:47:42 -0800

    CLI Chalk Update

--- src/commands/utility/serverHelpers.js
+++ src/commands/utility/serverHelpers.js
@@ -1,16 +1,16 @@
-const { exec }  = require('child_process');
+const { exec } = require('child_process');
 const opn = require('opn');
 const path = require('path');
 const PATH_TO_SERVER = path.join(__dirname, '../../server/server.js');
 const PORT = '3333';
-
-const openServer = (serverPath=PATH_TO_SERVER, port=PORT) => {
-  console.log("firing up");
+const chalk = require('chalk');
+const openServer = (serverPath = PATH_TO_SERVER, port = PORT) => {
+  console.log(chalk.cyan.bold("firing up"));
   exec(`node ${serverPath}`);
   opn(`http://localhost:${port}`)
 }
 exec(`echo hello`);
-console.log("sup");
+console.log(chalk.magenta.bold.underline("sup"));
 
 module.exports = {
   openServer,
