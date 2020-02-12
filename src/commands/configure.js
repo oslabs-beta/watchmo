c463cd19b0373e470858895e8c061aaac6b3d21c
Spencer Wyman <spencer.wyaman7@gmail.com>
2020-02-12 11:38:34 -0800

    added functionality to most watchmo configure flags

--- src/commands/configure.js
+++ src/commands/configure.js
@@ -31,24 +31,68 @@ function createProject(projectName) {
 }
 
 function changeEndpoint(projectName, endpoint) {
-  console.log("CHANGING ENDPOINT BEEP BOOP", projectName, endpoint);
+  const { configPath } = dataPaths(projectName);
+  const configObject = checkAndParseFile(configPath);
+  if (typeof endpoint === 'boolean') {
+    console.log("The '--endpoint' option requires a positional argument.");
+  } else {
+    configObject.endpoint = endpoint;
+    writeJSON(configPath, configObject);
+  }
 }
 
 function changeCategory(projectName, category, remove=false) {
-  console.log("CHANGING CATEGORY BEEP BOOP", projectName, category);
+  const { configPath } = dataPaths(projectName);
+  const configObject = checkAndParseFile(configPath);
+  if (typeof category === 'boolean') {
+    console.log("The '--category' option requires a positional argument.");
+  } else if (remove){
+    if (configObject.categories[category] !== undefined) {
+      delete configObject.categories[category];
+      writeJSON(configPath, configObject);
+    } else {
+      console.log(`The category ${category} you are trying to remove does not exist`);
+    }
+  } else {
+    if (configObject.categories[category] === undefined) {
+      configObject.categories[category] = {
+        queries:[],
+        frequency: -1,
+      };
+      writeJSON(configPath, configObject);
+    } else {
+      console.log(`The category ${category} already exists`);
+    }
+  }
 }
 
 function changeQuery(projectName, category, query, remove=false) {
-  console.log("CHANGING QUERY BEEP BOOP", projectName, category, query);
+  const { configPath } = dataPaths(projectName);
+  const configObject = checkAndParseFile(configPath);
+  if (typeof category === 'boolean' || typeof query === 'boolean') {
+    console.log("The '--query' '--category' options both require a positional argument.");
+  } else if (configObject.categories[category] === undefined){
+    console.log(`The category ${category} does not exist`);
+  } else if (!remove) {
+    configObject.categories[category].queries.push(query);
+    writeJSON(configPath, configObject);
+  } else if (remove) {
+    console.log("The CLI remove functionality for queries is not yet supported. Consider using the browser to remove queries.");
+  }
 }
 
-function configure(projectName, endpoint, category, query, remove=false) {
+function changeFrequency(projectName, frequency) {
+  console.log("The CLI frequency functionality is not yet supported. Consider using the browser to change the frequency");
+}
+
+function configure(projectName, endpoint, frequency, category, query, remove=false) {
   // not an if-else block so that you can configure endpoint AND category/query all at once if desired
   // default behavior, create project with the given project Name
-  if (!endpoint && !category) { createProject(projectName); }
+  if (!endpoint && !category && !frequency) { createProject(projectName); }
   if (endpoint) { changeEndpoint(projectName, endpoint); }
   if (category && !query) { changeCategory(projectName, category, remove); }
   if (category && query) { changeQuery(projectName, category, query, remove); }
+  if (category && query && frequency) { changeFrequency(projectName, frequency); }
 }
 
 
