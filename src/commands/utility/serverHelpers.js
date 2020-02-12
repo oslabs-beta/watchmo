a49569c9343b3904e32f3b547b8ebb94c1be50f3
Spencer Wyman <spencer.wyaman7@gmail.com>
2020-02-11 17:53:18 -0800

    changed log styling and moved watchmo logo to default command

--- src/commands/utility/serverHelpers.js
+++ src/commands/utility/serverHelpers.js
@@ -8,13 +8,6 @@ const chalk = require('chalk');
 const openServer = (serverPath = PATH_TO_SERVER, port = PORT) => {
   exec(`node ${serverPath}`);
   opn(`http://localhost:${port}`)
-  console.log(chalk.red.bold(`               _       _    ___  ___      `));
-  console.log(chalk.yellow.bold(`              | |     | |   |  \\/  |      `));
-  console.log(chalk.green.bold(`__      ____ _| |_ ___| |__ | .  . | ___  `));
-  console.log(chalk.blue.bold("\\ \\ /\\ / / _` | __/ __| '_ \\| |\\/| |/ _ \\ "));
-  console.log(chalk.cyan.bold(` \\ V  V / (_| | || (__| | | | |  | | (_) |`));
-  console.log(chalk.magenta.bold(`  \\_/\\_/ \\__,_|\\__\\___|_| |_\\_|  |_/\\___/ `));
-  console.log("                              ")
 }
 
 
